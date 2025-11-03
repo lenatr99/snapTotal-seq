1bf73b0140930d04a94161bab0ff5e7b4c0c0a84
lenatr99 <lt89715@student.uni-lj.si>
2025-11-03 10:06:18 +0100

    Small fix

--- 01_snapTotal_mapping.sh
+++ 01_snapTotal_mapping.sh
@@ -5,7 +5,7 @@ cutadapt -a CTCCACACATCCTCAACCATCACTCAC -o ${sample}/${sample}_adapter_trimmed_1
 cutadapt -u -10 -m 30 -o ${sample}/${sample}_UMItrimmed_1.fq.gz ${sample}/${sample}_adapter_trimmed_1.fq.gz > ${sample}/UMI_trimming_report.txt
 rm ${sample}/${sample}_adapter_trimmed_1.fq.gz
 
-# docker run --rm -it --platform linux/amd64 -v /Users/lenatrnovec/scTotalRNA:/data -w /data quay.io/biocontainers/star:2.7.11b--h43eeafb_1 STAR   --runMode genomeGenerate      --runThreadN 1   --genomeDir Dicty_STAR_index   --genomeFastaFiles genome.fa      --genomeSAindexNbases 12   --sjdbGTFfile genes.gtf   --sjdbGTFfeatureExon exon   --sjdbOverhang 100
+# docker run --rm -it --platform linux/amd64 -v /Users/lenatrnovec/scTotalRNA:/data -w /data quay.io/biocontainers/star:2.7.11b--h43eeafb_1 STAR   --runMode genomeGenerate      --runThreadN 8   --genomeDir Dicty_STAR_index   --genomeFastaFiles genome.fa      --genomeSAindexNbases 12   --sjdbGTFfile genes.gtf   --sjdbGTFfeatureExon exon   --sjdbOverhang 100
 
 docker run --rm -it --platform linux/amd64 -v /Users/lenatrnovec/scTotalRNA:/data -w /data quay.io/biocontainers/star:2.7.11b--h43eeafb_1 STAR --runThreadN 8 --runMode alignReads --genomeDir Dicty_STAR_index --outFilterMismatchNmax 5 --readFilesCommand zcat --readFilesIn ${sample}/${sample}_UMItrimmed_1.fq.gz --outSAMtype BAM Unsorted --outFileNamePrefix ${sample}/
 
